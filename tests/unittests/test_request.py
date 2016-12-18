ab0aafe089ac7074b019adbc4e951f4acc60e371
Herb Lainchbury <herb@dynamic-solutions.com>
2016-12-18 08:57:35 -0800

    add query_string and route request tests

--- tests/unittests/test_request.py
+++ tests/unittests/test_request.py
@@ -9,16 +9,34 @@ import logging
 from zoom.request import Request
 
 
-class TestRequest(unittest.TestCase):
+class TestCGIRequest(unittest.TestCase):
     """test request"""
 
-    def test_server_type(self):
-        env = {}
-        request = Request(env)
-        self.assertEqual(request.module, 'cgi')
+    def setUp(self):
+        self.server_type = 'cgi'
+        self.env = {
+            'REQUEST_URI': '/test/route',
+        }
 
-        env = {
+    def test_server_type(self):
+        request = Request(self.env)
+        self.assertEqual(request.module, self.server_type)
+
+    def test_query_string(self):
+        self.env['QUERY_STRING'] = 'name=joe&age=20'
+        request = Request(self.env)
+        self.assertEqual(request.data, {'name': 'joe', 'age': '20'})
+
+    def test_route(self):
+        request = Request(self.env)
+        self.assertEqual(request.route, ['test', 'route'])
+
+
+class TestWSGIRequest(TestCGIRequest):
+
+    def setUp(self):
+        self.server_type = 'wsgi'
+        self.env = {
             'wsgi.version': '1',
-            }
-        request = Request(env)
-        self.assertEqual(request.module, 'wsgi')
+            'PATH_INFO': '/test/route',
+        }
