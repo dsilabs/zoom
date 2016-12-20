5d27e4118071a0e1db075e37693fe30cb9f1a473
Herb Lainchbury <herb@dynamic-solutions.com>
2016-12-20 09:25:37 -0800

    refactor request module

--- zoom/request.py
+++ zoom/request.py
@@ -30,6 +30,7 @@ class Webvars(object):
         """gather query fields"""
 
         env = env or os.environ
+        get = env.get
 
         # switch to binary mode on windows systems
         # msvcrt and O_BINARY are only defined in Windows Python
@@ -42,9 +43,9 @@ class Webvars(object):
         except ImportError:
             pass
 
-        module = env.get('wsgi.version', None) and 'wsgi' or 'cgi'
+        module = get('wsgi.version', None) and 'wsgi' or 'cgi'
 
-        if env.get('REQUEST_METHOD', 'GET').upper() in ['GET']:
+        if get('REQUEST_METHOD', 'GET').upper() in ['GET']:
             cgi_fields = cgi.FieldStorage(environ=env, keep_blank_values=1)
         else:
             if module == 'wsgi':
@@ -66,10 +67,6 @@ class Webvars(object):
         items = {}
         for key in cgi_fields.keys():
 
-            # ignore legacy setup
-            if key == '_route':
-                continue
-
             if type(cgi_fields[key]) == list:
                 items[key] = [item.value for item in cgi_fields[key]]
             elif cgi_fields[key].filename:
@@ -86,6 +83,10 @@ class Webvars(object):
 
     def __repr__(self):
         return str(self)
+
+
+def get_web_vars(env, body):
+    return Webvars(env, body).__dict__
 
 
 def get_parent_dir():
@@ -115,7 +116,7 @@ class Request(object):
     @property
     def data(self):
         if not self.body_consumed:
-            return Webvars(self.env, self._body).__dict__
+            return get_web_vars(self.env, self._body)
         else:
             return {}
 
@@ -146,67 +147,54 @@ class Request(object):
                 return host.split(':')[0].split('www.')[-1:][0]
             return ''
 
-        self._data = None
+        get = env.get
+
         self._body = None
         self.body_consumed = False
 
-        path = env.get('PATH_INFO', env.get('REQUEST_URI', '').split('?')[0])
-        current_route = path != '/' and path.split('/')[1:] or []
-        cookies = get_cookies(env.get('HTTP_COOKIE'))
+        path = get('PATH_INFO', get('REQUEST_URI', '').split('?')[0])
 
-        module = env.get('wsgi.version', None) and 'wsgi' or 'cgi'
-
+        module = get('wsgi.version', None) and 'wsgi' or 'cgi'
         if module == 'wsgi':
-            server = (env.get('HTTP_HOST') or '').split(':')[0]
-            home = os.getcwd()
-            self._body = env.get('wsgi.input')
+            self.server = (get('HTTP_HOST') or '').split(':')[0]
+            self.home = os.getcwd()
+            self._body = get('wsgi.input')
+            self.wsgi_version = get('wsgi.version')
+            self.wsgi_urlscheme = get('wsgi.urlscheme')
+            self.wsgi_multiprocess = get('wsgi.multiprocess')
+            self.wsgi_multithread = get('wsgi.multithread')
+            self.wsgi_filewrapper = get('wsgi.filewrapper')
+            self.wsgi_runonce = get('wsgi.runonce')
+            self.wsgi_errors = get('wsgi.errors')
+            self.wsgi_input = get('wsgi.input')
+            self.mode = get('mod_wsgi.process_group', None) \
+                and 'daemon' or 'embedded'
         else:
-            server = env.get('SERVER_NAME', 'localhost')
-            home = os.path.dirname(env.get('SCRIPT_FILENAME', ''))
+            self.server = get('SERVER_NAME', 'localhost')
+            self.home = os.path.dirname(get('SCRIPT_FILENAME', ''))
             self._body = sys.stdin
 
-        instance = instance or get_parent_dir()
-        root = os.path.join(instance, 'sites', server)
-        mode = env.get('mod_wsgi.process_group', None) \
-            and 'daemon' or 'embedded'
-
-        # gather some commonly required environment variables
-        attributes = dict(
-            instance=instance,
-            path=path,
-            host=env.get('HTTP_HOST'),
-            domain=calc_domain(env.get('HTTP_HOST')),
-            uri=env.get('REQUEST_URI', 'index.py'),
-            query=env.get('QUERY_STRING'),
-            ip=env.get('REMOTE_ADDR'),  # deprecated
-            ip_address=env.get('REMOTE_ADDR'),
-            user=env.get('REMOTE_USER'),
-            cookies=cookies,
-            session_token=cookies.get(SESSION_COOKIE_NAME, None),
-            subject=cookies.get(SUBJECT_COOKIE_NAME, new_subject()),
-            port=env.get('SERVER_PORT'),
-            root=root,
-            server=server,
-            script=env.get('SCRIPT_FILENAME'),
-            home=home,
-            agent=env.get('HTTP_USER_AGENT'),
-            method=env.get('REQUEST_METHOD'),
-            module=module,
-            mode=mode,
-            protocol=env.get('HTTPS', 'off') == 'on' and 'https' or 'http',
-            referrer=env.get('HTTP_REFERER'),
-            wsgi_version=env.get('wsgi.version'),
-            wsgi_urlscheme=env.get('wsgi.urlscheme'),
-            wsgi_multiprocess=env.get('wsgi.multiprocess'),
-            wsgi_multithread=env.get('wsgi.multithread'),
-            wsgi_filewrapper=env.get('wsgi.filewrapper'),
-            wsgi_runonce=env.get('wsgi.runonce'),
-            wsgi_errors=env.get('wsgi.errors'),
-            wsgi_input=env.get('wsgi.input'),
-            route=current_route,
-            env=env
-        )
-        self.__dict__.update(attributes)
+        self.path = path
+        self.instance = instance or get_parent_dir()
+        self.root = os.path.join(self.instance, 'sites', self.server)
+        self.host = get('HTTP_HOST')
+        self.domain = calc_domain(get('HTTP_HOST'))
+        self.uri = get('REQUEST_URI', 'index.py')
+        self.query = get('QUERY_STRING')
+        self.ip_address = get('REMOTE_ADDR')
+        self.user = get('REMOTE_USER')
+        self.cookies = get_cookies(get('HTTP_COOKIE'))
+        self.session_token = self.cookies.get(SESSION_COOKIE_NAME, None)
+        self.subject = self.cookies.get(SUBJECT_COOKIE_NAME, new_subject())
+        self.port = get('SERVER_PORT')
+        self.script = get('SCRIPT_FILENAME')
+        self.agent = get('HTTP_USER_AGENT')
+        self.method = get('REQUEST_METHOD')
+        self.module = module
+        self.protocol = get('HTTPS', 'off') == 'on' and 'https' or 'http'
+        self.referrer = get('HTTP_REFERER')
+        self.route = path != '/' and path.split('/')[1:] or []
+        self.env = env
 
     def __str__(self):
         return '{\n%s\n}' % '\n'.join('  %s: %r' % (
