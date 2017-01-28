93b0aef7c7fc88546b09e70a573bec58093bcdde
Herb Lainchbury <herb@dynamic-solutions.com>
2017-01-28 13:38:31 -0800

    stub out Session.save method

--- zoom/session.py
+++ zoom/session.py
@@ -12,10 +12,15 @@ class Session(object):
 
     def __init__(self, token):
         self.token = token
-        logger = logging.getLogger(__name__)
-        logger.debug('session token: %r', token)
+        self.logger = logging.getLogger(__name__)
+        self.logger.debug('load session: %r', token)
+
+    def save(self):
+        self.logger.debug('save session: %r', self.token)
 
 
 def session_handler(request, handler, *rest):
     request.session = Session(request.session_token)
-    return handler(request, *rest)
+    response = handler(request, *rest)
+    request.session.save()
+    return response
