2f2ea3c738a39aed31dd8a870d4da0a73dbaff0e
Herb Lainchbury <herb@dynamic-solutions.com>
2017-03-14 11:11:26 -0700

    add path to site, check for missing sites, and empty database

--- zoom/site.py
+++ zoom/site.py
@@ -9,13 +9,20 @@ import zoom.config
 import zoom.database
 
 
+class EmptyDatabaseException(Exception):
+    pass
+
+
 class Site(object):
 
     def __init__(self, directory):
 
-        self.directory = directory
+        self.directory = self.path = directory
         self.config = zoom.config.Config(self.directory, 'site.ini')
         self.db = zoom.database.connect_database(self.config)
+
+        if self.db.get_tables() == []:
+            raise EmptyDatabaseException('Database is empty')
 
         logger = logging.getLogger(__name__)
         logger.debug('site loaded: %r', directory)
@@ -27,5 +34,7 @@ def site_handler(request, handler, *rest):
         logger = logging.getLogger(__name__)
         logger.debug('site directory: %r', site_directory)
         request.site = Site(site_directory)
+    else:
+        raise Exception('site {!r} does not exist'.format(site_directory))
 
     return handler(request, *rest)
