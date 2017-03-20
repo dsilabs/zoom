df1c16c0b1472a803c2ecb0794c62a8e2f1a8e86
Herb Lainchbury <herb@dynamic-solutions.com>
2017-03-20 10:27:20 -0700

    add components module

---
+++ zoom/components.py
@@ -0,0 +1,39 @@
+"""
+    zoom.components
+"""
+
+from zoom.utils import id_for
+from zoom.helpers import url_for
+from zoom.html import a, div, ul
+
+
+def as_actions(items):
+    """
+        returns actions
+
+        >>> as_actions(['New'])
+        '<div class="actions"><ul><li><a class="action" id="new-action" href="/new">New</a></li></ul></div>'
+        >>> as_actions(['New','Delete'])
+        '<div class="actions"><ul><li><a class="action" id="delete-action" href="/delete">Delete</a></li><li><a class="action" id="new-action" href="/new">New</a></li></ul></div>'
+
+    """
+    if not items:
+        return ''
+    result = []
+    for item in reversed(items):
+        if type(item) == str:
+            text = item
+            url = url_for(id_for(item))
+        elif hasattr(item, '__iter__'):
+            text, url = item[:2]
+        else:
+            raise Exception('actions require str or (str,url)')
+        result.append(
+            a(
+                text,
+                Class='action',
+                id=id_for(text)+'-action',
+                href=url
+            )
+        )
+    return div(ul(result), Class='actions')
