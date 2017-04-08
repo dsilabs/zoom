3c72e2cb2ccffc7dbec37cdf62d8ff6867165098
Herb Lainchbury <herb@dynamic-solutions.com>
2017-04-08 13:35:18 -0700

    Add Form class

--- zoom/forms.py
+++ zoom/forms.py
@@ -5,18 +5,31 @@
 import uuid
 
 from zoom.helpers import tag_for
+from zoom.fields import Fields
 import zoom.html as html
 
 
 def form_for(content=None, **kwargs):
-    """returns a form with optional hidden values"""
+    """returns a form with optional hidden values
 
-    name = kwargs.get('form_name', 'zoom_form')
-    method = kwargs.pop('method', 'POST')
-    action = kwargs.pop('action', '<dz:request_path>')
+    >>> print(form_for('test'))
+    <form action="<dz:request_path>" enctype="application/x-www-form-urlencoded" id="zoom_form" method="POST" name="zoom_form">
+    <input name="csrf_token" type="hidden" value="<dz:csrf_token>" />
+    test
+    </form>
+
+
+    """
+
+    params = kwargs.copy()
+    name = params.pop('form_name', 'zoom_form')
+    id = params.pop('id', 'zoom_form')
+    method = params.pop('method', 'POST')
+    action = params.pop('action', '<dz:request_path>')
+    enctype = params.pop('enctype', 'application/x-www-form-urlencoded')
 
     t = []
-    for key, value in kwargs.items():
+    for key, value in params.items():
         t.append(
             html.hidden(name=key, value=value)
         )
@@ -28,11 +41,12 @@ def form_for(content=None, **kwargs):
 
     return html.tag(
         'form',
-        ''.join(t + [content or '']),
+        '\n' + '\n'.join(t + [content or '']) + '\n',
         action=action,
         name=name,
         id=name,
         method=method,
+        enctype=enctype,
     )
 
 
@@ -56,6 +70,61 @@ def csrf_token(session):
     if not hasattr(session, 'csrf_token'):
         session.csrf_token = uuid.uuid4().hex
     return session.csrf_token
+
+
+class Form(Fields):
+    """An HTML form
+
+    >>> from zoom.fields import TextField
+    >>> form = Form(TextField("Name"))
+    >>> print(form.edit())
+    <form action="" enctype="application/x-www-form-urlencoded" id="zoom_form" method="POST" name="zoom_form">
+    <input name="csrf_token" type="hidden" value="<dz:csrf_token>" />
+    <div class="field">
+      <div class="field_label">Name</div>
+      <div class="field_edit"><table class="transparent">
+        <tr>
+            <td nowrap><input class="text_field" id="name" maxlength="40" name="name" size="40" type="text" value="" /></td>
+            <td>
+                <div class="hint"></div>
+            </td>
+        </tr>
+    </table>
+    </div>
+    </div>
+    <BLANKLINE>
+    <BLANKLINE>
+    """
+
+    def __init__(self, *args, **kwargs):
+
+        if len(args) == 1 and isinstance(args[0], (list, set, tuple)):
+            self.fields = args[0]
+        else:
+            self.fields = list(args)
+
+        self.enctype = 'application/x-www-form-urlencoded'
+        for field in self.fields:
+            try:
+                if field.requires_multipart_form():
+                    self.enctype = 'multipart/form-data'
+            except AttributeError:
+                pass
+
+        self.action = kwargs.get('action', '')
+        self.form_name = kwargs.get('form_name', 'zoom_form')
+        self.method = kwargs.get('method', 'POST')
+
+    def edit(self):
+        fields = '\n  '.join([field.edit() for field in self.fields])
+        return form(
+            fields,
+            action=self.action,
+            id=self.form_name,
+            form_name=self.form_name,
+            method=self.method,
+            enctype=self.enctype,
+        )
 
 
 def helpers(request):
