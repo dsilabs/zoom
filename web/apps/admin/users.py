280e0b5c45c328405610dbf05afe3e06d766bf22
Herb Lainchbury <herb@dynamic-solutions.com>
2017-06-28 15:02:37 -0700

    add ability to reset passwords and activate and deactivate users

--- web/apps/admin/users.py
+++ web/apps/admin/users.py
@@ -3,10 +3,12 @@
 """
 
 from zoom.components import success, error
-from zoom.collect import Collection, CollectionController
+from zoom.collect import Collection, CollectionView, CollectionController
+from zoom.context import context
 from zoom.forms import Form
+from zoom.page import page
 from zoom.users import User, Users
-from zoom.tools import now
+from zoom.tools import now, home
 import zoom.validators as v
 import zoom.fields as f
 
@@ -57,6 +59,38 @@ def user_fields(request):
     return f.Fields(personal_fields, account_fields, security_fields)
 
 
+def get_reset_password_form(key):
+    reset_password_form = Form(
+        f.TextField('New Password', v.required),
+        f.ButtonField('Save Password', cancel='/admin/users/' + key)
+    )
+    return reset_password_form
+
+
+class UserCollectionView(CollectionView):
+
+    def show(self, key):
+        page = CollectionView.show(self, key)
+        page.actions.insert(0, 'Reset Password')
+        user = context.site.users.first(username=key)
+        if user.is_active:
+            page.actions.insert(0, 'Deactivate')
+        else:
+            page.actions.insert(0, 'Activate')
+        return page
+
+    def reset_password(self, key, **kwargs):
+        user = context.site.users.first(username=key)
+        if user:
+            msg = 'Reset password for %s (%s)<br><br>' % (user.full_name, user.username)
+            form = get_reset_password_form(key)
+            form.validate(kwargs)
+            content = msg + form.edit()
+        else:
+            content = 'Error locating user %r' % key
+        return page(content, title='Reset Password')
+
+
 class UserCollectionController(CollectionController):
 
     def before_update(self, record):
@@ -65,15 +99,38 @@ class UserCollectionController(CollectionController):
         record['updated_by'] = self.collection.user._id
         update_user_groups(record)
 
+    def save_password_button(self, key, *args, **data):
+        form = get_reset_password_form(key)
+        if form.validate(data):
+            user = context.site.users.first(username=key)
+            if user:
+                new_password = form.evaluate()['new_password']
+                user.set_password(new_password)
+                success('password updated')
+                return home('users/' + key)
+
+    def activate(self, key):
+        user = context.site.users.first(username=key)
+        user.activate()
+        user.save()
+        return home('users/' + key)
+
+    def deactivate(self, key):
+        user = context.site.users.first(username=key)
+        user.deactivate()
+        user.save()
+        return home('users/' + key)
+
 
 def main(route, request):
     db = request.site.db
     users = Users(db)
     fields = user_fields(request)
-    columns = 'link', 'username', 'phone', 'email', 'username', 'status', 'updated', 'updated_by'
+    columns = 'link', 'username', 'phone', 'email', 'status', 'updated', 'updated_by'
     return Collection(
         fields,
         model=User,
+        view=UserCollectionView,
         controller=UserCollectionController,
         store=users,
         item_name='user',
