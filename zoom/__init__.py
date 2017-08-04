3d57ac16fcd350f72943ed6914c76efd0ced720c
Herb Lainchbury <herb@dynamic-solutions.com>
2017-08-04 15:59:24 -0700

    Make some frequently used features easier to use

---
+++ zoom/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#   ____
+#     /     _     _    _   _
+#    /    /   \ /   \ | \_/ |
+#   /___  \ _ / \ _ / |     |
+
+"""
+    Zoom Web Framework
+"""
+
+import zoom.database
+import zoom.records
+import zoom.store
+
+from .browse import browse
+from .component import Component
+from .context import context as system
+from .forms import form, Form
+from .mvc import View, Controller
+from .page import page, Page
+from .packages import requires
