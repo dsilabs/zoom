3beeb3102107ee799b14bc86f9109450e15b4525
Herb Lainchbury <herb@dynamic-solutions.com>
2017-08-18 09:43:07 -0700

    Promote redirect_to function to top level

--- zoom/__init__.py
+++ zoom/__init__.py
@@ -21,4 +21,4 @@ from .forms import form, Form
 from .mvc import View, Controller
 from .page import page, Page
 from .packages import requires
-from .tools import home
+from .tools import home, redirect_to
