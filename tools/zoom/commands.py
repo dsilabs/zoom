a382707f921465f7ac1e50e16634247029e1e748
Herb Lainchbury <herb@dynamic-solutions.com>
2017-08-19 22:36:44 -0700

    Add setup command to CLI

--- tools/zoom/commands.py
+++ tools/zoom/commands.py
@@ -11,6 +11,7 @@
 import logging
 import os
 import sys
+import shutil
 from argparse import ArgumentParser
 
 from database import database
@@ -20,7 +21,47 @@ __all__ = [
     'server',
     'database',
     'new',
+    'setup',
 ]
+
+def setup(name=None):
+    """set up a new Zoom work area"""
+
+    join = os.path.join
+
+    parser = ArgumentParser(
+        description='set up a new work area',
+        usage='zoom setup [options] directory'
+        )
+
+    parser.add_argument('directory', nargs=1)
+    args = parser.parse_args()
+
+    if os.path.exists(args.directory[0]):
+        print('error: directory exists')
+        sys.exit(-1)
+
+    # locate the bundled work area
+    src = os.path.abspath(
+        os.path.join(os.path.split(__file__)[0], '../../web'),
+    )
+
+    # create the new directory
+    dst = args.directory[0]
+    os.mkdir(dst)
+
+    # create an empty apps directory for new app developement
+    os.mkdir(join(dst, 'apps'))
+
+    # create the themes directory for new theme developmenet
+    os.mkdir(join(dst, 'themes'))
+    # copy default theme
+    shutil.copytree(
+        join(src, 'themes', 'default'), join(dst, 'themes', 'default')
+    )
+
+    # copy sites directory
+    shutil.copytree(join(src, 'sites'), join(dst, 'sites'))
 
 
 def server(instance='.'):
