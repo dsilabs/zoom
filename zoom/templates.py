0c68a2536bf6afbd5db07f9ef810369e5fb04972
Herb Lainchbury <herb@dynamic-solutions.com>
2018-01-16 08:17:35 -0800

    Remove old code

--- zoom/templates.py
+++ zoom/templates.py
@@ -2,17 +2,6 @@
     templates.zoom
 """
 
-from zoom.page import page
-
-
-def app_not_found(request):
-    # pylint: disable=unused-argument
-    return """
-    <h1>System Message</h1>
-    <p>This site is currently under construction.</p>
-    """
-
-
 page_not_found = """
     <div class="jumbotron">
         <h1>Page Not Found</h1>
