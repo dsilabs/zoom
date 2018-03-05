aef2a105853a7a75f3ec9917908baeb6eb28d3a7
Herb Lainchbury <herb@dynamic-solutions.com>
2018-03-04 19:24:10 -0800

    Add tools tests

--- tests/unittests/test_tools.py
+++ tests/unittests/test_tools.py
@@ -20,11 +20,61 @@ class TestFill(unittest.TestCase):
         template = load('default')
         self.assertTrue('<html>' in template)
 
+    def test_load_template_source_note_name(self):
+        zoom.system.request = zoom.utils.Bunch(site=zoom.sites.Site())
+        load = zoom.tools.load_template
+        template = load('default')
+        self.assertTrue('source: default' in template)
+
+    def test_load_template_source_note_path(self):
+        site=zoom.sites.Site()
+        site.theme_comments = 'path'
+        zoom.system.request = zoom.utils.Bunch(site=site)
+        load = zoom.tools.load_template
+        template = load('default')
+        self.assertFalse('source: default' in template)
+        self.assertTrue('source:' in template)
+        self.assertTrue('themes/default/default.html' in template)
+
+    def test_load_template_source_none(self):
+        site=zoom.sites.Site()
+        site.theme_comments = 'none'
+        zoom.system.request = zoom.utils.Bunch(site=site)
+        load = zoom.tools.load_template
+        template = load('default')
+        self.assertFalse('source: default' in template)
+        self.assertFalse('source:' in template)
+        self.assertFalse('themes/default/default.html' in template)
+
+    def test_load_template_mixed_case(self):
+        zoom.system.request = zoom.utils.Bunch(site=zoom.sites.Site())
+        load = zoom.tools.load_template
+        template = load('Default')
+        self.assertTrue('<html>' in template)
+
     def test_load_template_missing(self):
         zoom.system.request = zoom.utils.Bunch(site=zoom.sites.Site())
         load = zoom.tools.load_template
         template = load('notthere')
         self.assertEqual('<!-- template missing -->', template)
+
+    def test_load_template_invalid_path(self):
+        zoom.system.request = zoom.utils.Bunch(site=zoom.sites.Site())
+        load = zoom.tools.load_template
+        self.assertRaises(
+            Exception,
+            load, '/default'
+        )
+
+    def test_load_content(self):
+        zoom.system.site = zoom.sites.Site()
+        content = zoom.tools.load_content(zoom.tools.zoompath('README'))
+        self.assertTrue('<h1 id="zoom">Zoom</h1>' in content)
+
+    def test_load_content_missing(self):
+        zoom.system.site = zoom.sites.Site()
+        load = zoom.tools.load_content
+        self.assertRaises(FileNotFoundError, load, 'noREADME')
 
     def test_load_template_missing_and_default(self):
         zoom.system.request = zoom.utils.Bunch(site=zoom.sites.Site())
