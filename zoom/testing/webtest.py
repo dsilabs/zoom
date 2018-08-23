ed6d1815da43311b15f9b2b69f7b944786feaf31
Herb Lainchbury <herb@dynamic-solutions.com>
2018-08-23 05:18:46 -0700

    add assertDoesNotContain to webtests

--- zoom/testing/webtest.py
+++ zoom/testing/webtest.py
@@ -220,6 +220,13 @@ class WebdriverTestPrimitives(unittest.TestCase):
             msg = 'page does not contain {!r}'.format(text)
             raise self.failureException(msg)
 
+    def assertDoesNotContain(self, text):
+        if self.contains(text):
+            test_name = unittest.TestCase.id(self)
+            self.driver.save_screenshot('%s-error_screen.png' % test_name)
+            msg = 'page contains {!r}'.format(text)
+            raise self.failureException(msg)
+
     @property
     def page_source(self):
         return self.driver.page_source
