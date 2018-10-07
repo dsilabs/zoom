460ac0009d61be0381aeabb9580c7e2f4aed7252
Herb Lainchbury <herb@dynamic-solutions.com>
2018-10-06 18:23:26 -0700

    reorder menu items

--- web/apps/admin/app.py
+++ web/apps/admin/app.py
@@ -5,4 +5,8 @@
 from zoom.apps import App
 
 app = App()
-app.menu = 'Overview', 'Apps', 'Users', 'Groups', 'Mail', 'Activity', 'Requests', 'Errors', 'Audit', 'Configuration', 'About'
+app.menu = (
+    'Overview', 'Apps', 'Users', 'Groups', 'Mail',
+    'Requests', 'Activity', 'Errors', 'Audit',
+    'Configuration', 'About'
+)
