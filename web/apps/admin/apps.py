d50f62096a1389412f6931a0a59853b552af0594
Herb Lainchbury <herb@dynamic-solutions.com>
2018-11-07 01:52:57 -0800

    add groups to apps page

--- web/apps/admin/apps.py
+++ web/apps/admin/apps.py
@@ -8,17 +8,36 @@ class AppsController(zoom.Controller):
 
     def index(self):
 
+        def get_group_link(group_id):
+            group = groups.get(group_id)
+            return group.link if group else zoom.html.span(
+                'missing',
+                title='unknown subgroup id {!r}'.format(group_id)
+            )
+
         def fmt(app):
             name = app.name
             group_name = 'a_' + app.name
-            has_group = zoom.system.site.groups.find(name=group_name)
-            return [app.name,  app.title, app.path, 'yes' if has_group else 'no']
+            app_group = zoom.system.site.groups.first(name=group_name)
+            groups = ', '.join(
+                get_group_link(s) for g, s in subgroups if g == app_group._id
+            ) if app_group else ''
+            return [
+                app.name,
+                app.title,
+                app.path,
+                groups,
+            ]
 
+        db = zoom.system.site.db
+        subgroups = list(db('select * from subgroups'))
+        groups = dict((g._id, g) for g in zoom.system.site.groups)
         data = [fmt(app) for app in sorted(zoom.system.site.apps, key=lambda a: a.name)]
         content = zoom.browse(
             data,
-            labels=['App', 'Title', 'Path', 'Group']
+            labels=['App', 'Title', 'Path', 'Groups']
         )
         return zoom.page(content, title='Apps')
 
 main = zoom.dispatch(AppsController)
+
