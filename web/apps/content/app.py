85c720971c9f9222a8384d7244c211635c9846dd
Herb Lainchbury <herb@dynamic-solutions.com>
2018-12-24 11:22:51 -0800

    add policy to content app

--- web/apps/content/app.py
+++ web/apps/content/app.py
@@ -4,12 +4,16 @@
 
 import logging
 
+import zoom
 from zoom.apps import App
 
 
 class CustomApp(App):
     """Content App"""
     # pylint: disable=R0903
+
+    def allows(self, user, action):
+        return user.is_member('managers')
 
     def __call__(self, request):
         logger = logging.getLogger(__name__)
@@ -29,7 +33,7 @@ class CustomApp(App):
             request.route = request.path.split('/')
             return App.__call__(self, request)
 
-        elif request.route and request.route[0] == 'content':
+        elif request.route and request.route[0] == 'content' and zoom.system.user.can('edit', self):
             # this is a request to manage site content
             self.menu = ['Overview', 'Pages', 'Snippets']
             return App.__call__(self, request)
