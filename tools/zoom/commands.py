55f5e66dc7d30351ded0b2c455b5dc2d0d566bb2
Herb Lainchbury <herb@dynamic-solutions.com>
2019-05-16 15:18:01 -0700

    move setup command into module in cli directory

--- tools/zoom/commands.py
+++ tools/zoom/commands.py
@@ -14,6 +14,7 @@ import sys
 import shutil
 from argparse import ArgumentParser
 
+from zoom.cli.setup import setup
 from database import database
 from new import new
 
@@ -23,46 +24,6 @@ __all__ = [
     'new',
     'setup',
 ]
-
-def setup(name=None):
-    """set up a new Zoom instance"""
-
-    join = os.path.join
-
-    parser = ArgumentParser(
-        description='set up a new Zoom instance',
-        usage='zoom setup [options] directory'
-        )
-
-    parser.add_argument('directory', nargs=1)
-    args = parser.parse_args()
-
-    if os.path.exists(args.directory[0]):
-        print('warning: directory exists')
-        sys.exit(-1)
-
-    # locate the bundled work area
-    src = os.path.abspath(
-        os.path.join(os.path.split(__file__)[0], '../../web'),
-    )
-
-    # create the new directory
-    dst = args.directory[0]
-    os.mkdir(dst)
-
-    # create an empty apps directory for new app developement
-    os.mkdir(join(dst, 'apps'))
-
-    # create the themes directory for new theme developmenet
-    os.mkdir(join(dst, 'themes'))
-    # copy default theme
-    shutil.copytree(
-        join(src, 'themes', 'default'), join(dst, 'themes', 'default')
-    )
-
-    # copy sites directory
-    shutil.copytree(join(src, 'sites'), join(dst, 'sites'))
-
 
 def server(instance='.'):
     """run an instance using Python's builtin HTTP server"""
