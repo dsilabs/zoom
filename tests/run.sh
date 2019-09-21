f301ba9444734a3e77fd0ffa610b49aad65e991e
Herb Lainchbury <herb@dynamic-solutions.com>
2019-09-21 14:13:35 -0700

    add fix typo

--- tests/run.sh
+++ tests/run.sh
@@ -34,4 +34,4 @@ python3.7 bin/zoom server -p 8000 web &
 
 cat web/sites/localhost/site.ini
 
-nosetests --with-coverage --cover-package=zoom -vx --exclude-dir=zoom/testing --exclude=tests/sidetests
+nosetests --with-coverage --cover-package=zoom -vx --exclude-dir=zoom/testing --exclude-dir=tests/sidetests
