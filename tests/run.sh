83b4255d4ebc0a24861ba698254273237a1aa4ed
Herb Lainchbury <herb@dynamic-solutions.com>
2019-09-21 10:57:11 -0700

    add chrome driver

--- tests/run.sh
+++ tests/run.sh
@@ -15,5 +15,19 @@ echo "host=mariadb" >> web/sites/localhost/site.ini
 echo "user=root" >> web/sites/localhost/site.ini
 echo "password=root" >> web/sites/localhost/site.ini
 
+# install google chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get -y update
+apt-get install -y google-chrome-stable
+
+# install chromedriver
+apt-get install -yqq unzip
+wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+# set display port to avoid crash
+ENV DISPLAY=:99
+
 cat web/sites/localhost/site.ini
 nosetests --with-coverage --cover-package=zoom -vx zoom tests/unittests --exclude-dir=zoom/
