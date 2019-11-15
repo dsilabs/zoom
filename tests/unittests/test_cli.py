4189494143e78cf8c681cc3a596d42e25cb06537
robinsax <robin@stonebreakerdesigns.com>
2019-11-15 13:33:28 -0800

    Refactor CLI unit tests, add pip install test case

--- tests/unittests/test_cli.py
+++ tests/unittests/test_cli.py
@@ -8,7 +8,6 @@ import shutil
 import unittest
 
 from subprocess import Popen, PIPE
-from requests import get
 
 from zoom.cli import __doc__ as MAIN_USAGE
 from zoom.cli.new import new as new_handler
@@ -20,13 +19,16 @@ def deformat_str(string):
     as program output across platforms."""
     return re.sub(r'\s+', str(), string)
 
-def agnostic_contains(output, content):
-    return deformat_str(content) in deformat_str(output)
+def agnostic_contains(source, content):
+    """Whether the given source contains the given content, agnostic of
+    whitespace (which is platform dependent in the context of stdout)."""
+    return deformat_str(content) in deformat_str(source)
 
-def invoke(*args, stdin=str(), return_proc=False):
+def invoke(*args, stdin=str(), return_proc=False, **kwargs):
     process = Popen(
-        ' '.join(('python zoom', *args)), shell=True, 
-        stdin=PIPE, stdout=PIPE, stderr=PIPE
+        ' '.join(('python3', *args)), 
+        shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, 
+        **kwargs
     )
     if return_proc:
         return process
@@ -46,41 +48,77 @@ class TestCLI(unittest.TestCase):
         shutil.rmtree(TEST_DIR)
 
     def test_usage(self):
-        code, out, err = invoke()
+        code, out, err = invoke('zoom')
         self.assertTrue(code, 'Non-zero exit')
         self.assertTrue(not out, 'No output')
         self.assertTrue(agnostic_contains(err, MAIN_USAGE), 'Usage provided')
 
     def test_help(self):
-        code, out, err = invoke('-h')
+        code, out, err = invoke('zoom -h')
         self.assertTrue(not err and not code, 'No error')
         self.assertTrue(agnostic_contains(out, MAIN_USAGE), 'Usage provided')
 
     def test_command_help(self):
-        code, out, err = invoke('-h new')
+        code, out, err = invoke('zoom -h new')
         self.assertTrue(not err and not code, 'No error')
-        self.assertTrue(agnostic_contains(out, new_handler.__doc__), 'Help provided')
+        self.assertTrue(
+            agnostic_contains(out, new_handler.__doc__), 
+            'Help provided'
+        )
 
     def test_command_usage_error(self):
-        code, out, err = invoke('new invalid command input')
+        code, out, err = invoke('zoom new invalid command input')
         self.assertTrue(code, 'Non-zero exit')
         self.assertTrue(not out, 'No output')
-        self.assertTrue(agnostic_contains(new_handler.__doc__, err), 'Usage provided')
+        self.assertTrue(
+            agnostic_contains(new_handler.__doc__, err),
+            'Usage provided'
+        )
 
     def test_new_app(self):
         path0 = os.path.join(TEST_DIR, 'app0')
-        code, out, err = invoke('new app "%s"'%path0)
+        code, out, err = invoke('zoom new app "%s"'%path0)
         self.assertTrue(not code, 'Process succeeds')
-        self.assertTrue(os.path.exists(path0) and os.path.isdir(path0), 'Parent directory created')
-        self.assertTrue(os.path.isfile(path0 + '/app.py'), 'Files created')
+        self.assertTrue(
+            os.path.exists(path0) and os.path.isdir(path0), 
+            'Parent directory created'
+        )
+        self.assertTrue(
+            os.path.isfile(path0 + '/app.py'), 'Files created'
+        )
 
-    def test_server(self):
-        server_proc = invoke('server -p 7999', return_proc=True)
-        try:
-            get('http://localhost:7999')
-        except:
-            self.assertTrue(False, 'Server responds')
-        
-        server_proc.kill()
-        # Dispatch another request so server_proc stops blocking and catches the SIGKILL.
-        get('http://localhost:7999')
+    def test_pip_install(self):
+        lib_path = os.path.join(TEST_DIR, 'libs')
+        cwd = os.path.abspath('.')
+        os.mkdir(lib_path)
+        code, out, err = invoke(
+            '-m pip install --target=%s %s'%(lib_path, cwd), 
+            cwd=TEST_DIR
+        )
+        self.assertTrue(not code, 'No error code')
+
+        # Create and environment that will cause our installed Zoom version
+        # to be executed.
+        child_env = os.environ.copy()
+        child_env['PYTHONPATH'] = lib_path
+
+        # Perform a sanity check that we're using the installed version, not
+        # this (the repo) one.
+        location_check = invoke(
+            '-c "import zoom;import os;print(os.path.abspath(zoom.__file__))"',
+            env=child_env, cwd=os.path.dirname(cwd)
+        )
+        self.assertTrue(
+            lib_path in location_check[1], 
+            'Sanity check; correct lib. path used for child env.'
+        )
+
+        # Invoke the installed zoom version to ensure it is linked properly.
+        code, out, err = invoke(
+            '-m zoom -h', env=child_env, cwd=os.path.dirname(cwd)
+        )
+
+        self.assertTrue(
+            not code and agnostic_contains(out, MAIN_USAGE), 
+            'Module-style invocation works'
+        )
