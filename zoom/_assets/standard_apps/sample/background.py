8c5edc158b753ea5f8eef66158f217b872fcbda5
Herb Lainchbury <herb@dynamic-solutions.com>
2020-04-29 20:33:13 -0700

    refactore sample background process

--- zoom/_assets/standard_apps/sample/background.py
+++ zoom/_assets/standard_apps/sample/background.py
@@ -1,49 +1,54 @@
-import time
-from datetime import datetime
+"""
+
+    sample background process
+
+    This module runs a background process and provides the page where
+    the results of that background process can be viewed.
+
+"""
+
 
 import zoom
-from zoom.context import context
-from zoom.store import Entity, store_of
 from zoom.background import cron
-from zoom.mvc import View
 
-class SampleAppTickEvent(Entity):
 
-    @classmethod
-    def store(cls):
-        return store_of(cls)
+class SampleAppTickEvent(zoom.Record):
 
-    @classmethod
-    def get_all(cls):
-        return cls.store().find(site=context.site.name)
+    @property
+    def when(self):
+        return zoom.helpers.when(self.timestamp)
 
-    @classmethod
-    def get_last(cls):
-        all_events = cls.get_all()
-        last = None
-        for event in all_events:
-            if not last or event.count > last.count:
-                last = event
-        return last
+Event = SampleAppTickEvent
+
 
 @cron('* * * * *')
 def tick():
-    existing = SampleAppTickEvent.get_last()
-    count = existing.count + 1 if existing else 0
-    return SampleAppTickEvent.store().put(SampleAppTickEvent(
-        time=datetime.now().isoformat(),
-        count=count,
-        site=context.site.name
-    ))
+    """store the current timestamp"""
+    site = zoom.system.site
+    events = zoom.store_of(Event)
+    event = events.first(site=site.name) or Event(site=site.name, counter=1)
+    event.timestamp = zoom.tools.now()
+    event.counter += 1
+    event_id = events.put(event)
+    return event_id
 
-class TickEventView(View):
+
+class TickEventView(zoom.View):
 
     def index(self):
-        last = SampleAppTickEvent.get_last()
-        return zoom.page('<h1>Background ticks</h1><p>' + ('''
-            Last tick (#%d) was at %s.
-        '''%(last.count, last.time) if last else '''
-            No ticks yet. Background jobs probably aren't set up to run.
-        ''') + '</p>')
+        site = zoom.system.site
+        events = zoom.store_of(Event)
+        event = events.first(site=site.name)
 
-view = TickEventView()
+        if event:
+            msg = 'Tick {event.counter} happened {event.when}'
+            message = msg.format(event=event)
+        else:
+            message = 'No ticks yet.  Background process may not be active.'
+
+        return zoom.page(
+            message,
+            title='Background'
+        )
+
+main = zoom.dispatch(TickEventView)
