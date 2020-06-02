8854e989c30cbeafdd7943460fd34dde597f26ee
Herb Lainchbury <herb@dynamic-solutions.com>
2020-06-02 14:15:16 -0700

    fix fallback static location

--- zoom/middleware.py
+++ zoom/middleware.py
@@ -262,7 +262,7 @@ def serve_static(request, handler, *rest):
             join(request.instance, 'static'),
             join(request.instance, 'www', 'static'),
             join(request.instance, '..', 'static'),
-            zoom.tools.zoompath('zoom', '_assets', 'www', 'static'),
+            zoom.tools.zoompath('zoom', '_assets', 'web', 'www', 'static'),
         ]))
         for location in locations:
             pathname = join(location, *request.route[1:])
