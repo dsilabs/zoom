3dc52f81ad3ed467bee4398e5483bf46d696118c
Herb Lainchbury <herb@dynamic-solutions.com>
2020-07-03 09:25:25 -0700

    add elapsed

--- zoom/components/instances/__init__.py
+++ zoom/components/instances/__init__.py
@@ -22,6 +22,7 @@ def get_info_page(request):
         date=datetime.datetime.now(),
         version=zoom.__version__,
         css=css,
+        elapsed=request.elapsed * 1000
     )
 
     content = html.format(**values)
