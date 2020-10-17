a3cf631d88557220d0fda958373e28772c31d815
mike <mike@dynamic-solutions.com>
2020-10-16 17:30:44 -0700

    Require c3 in pivot package

--- zoom/packages.py
+++ zoom/packages.py
@@ -144,8 +144,6 @@ default_packages = {
     'pivot-table': {
         'libs': [
             "//cdnjs.cloudflare.com/ajax/libs/pivottable/2.23.0/pivot.min.js",
-            "//cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js",
-            "//cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js",
             "//cdnjs.cloudflare.com/ajax/libs/pivottable/2.23.0/c3_renderers.min.js",
             "//cdnjs.cloudflare.com/ajax/libs/pivottable/2.23.0/d3_renderers.min.js",
             "//cdnjs.cloudflare.com/ajax/libs/pivottable/2.23.0/pivot.cs.min.js",
@@ -155,6 +153,9 @@ default_packages = {
         'styles': [
             '//cdnjs.cloudflare.com/ajax/libs/pivottable/2.23.0/pivot.min.css'
         ],
+        'requires': [
+            'c3',
+        ]
     },
     'spin': {
         'libs': [
