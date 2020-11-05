aca7bf8a67854ad2d7fce62cef2268607b25f95a
Herb Lainchbury <herb@dynamic-solutions.com>
2020-11-04 16:41:50 -0800

    add jquery requirement for datatables pacakge

--- zoom/packages.py
+++ zoom/packages.py
@@ -80,6 +80,9 @@ default_packages = {
             ],
         'styles': [
             '//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css',
+        ],
+        'requires': [
+            'jquery'
         ]
     },
     'datatables.buttons': {
