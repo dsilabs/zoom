88f197404a1e0fd08e905a7cd86647f312965f75
Herb Lainchbury <herb@dynamic-solutions.com>
2020-12-18 23:24:00 -0800

    add csrf_token to api app

--- zoom/_assets/standard_apps/api/app.py
+++ zoom/_assets/standard_apps/api/app.py
@@ -2,6 +2,13 @@
     zoom api
 """
 
-def app(_):
-    return '{"status":"200 OK"}'
+from zoom.utils import create_csrf_token
+
+def app(request):
+    token = create_csrf_token()
+    request.session.csrf_token = token
+    return dict(
+        status='200 OK',
+        csrf_token=token
+    )
 
