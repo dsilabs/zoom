41ce73dafc3a5a4356a296db006638ce33103132
Herb Lainchbury <herb@dynamic-solutions.com>
2022-07-07 17:07:21 -0700

    add basic background tests

--- tests/unittests/test_background.py
+++ tests/unittests/test_background.py
@@ -12,11 +12,6 @@ class TestBackground(unittest.TestCase):
 
     def setUp(self):
         zoom.system.site = zoom.sites.Site()
-        # zoom.system.request = zoom.utils.Bunch(
-        #     site=zoom.system.site,
-        #     app=zoom.utils.Bunch(templates_paths=[], url='/app1')
-        # )
-        # zoom.system.providers = [{}]
 
     def test_purge(self):
 
@@ -47,3 +42,31 @@ class TestBackground(unittest.TestCase):
         purge_old_job_results()
 
         self.assertEqual(len(job_log), 100)
+
+    def test_instance_jobs_exist(self):
+        instance = zoom.instances.Instance()
+        test_apps_path = zoom.tools.zoompath('tests', 'unittests', 'apps')
+        names = []
+        for site in instance.sites.values():
+            site.apps_paths.append(test_apps_path)
+            for job in site.background_jobs:
+                names.append(job.qualified_name)
+            del site.apps_paths[-1]
+        self.assertEqual(
+            names,
+            [
+                'localhost/sample:tick',
+                'localhost/test2:hello',
+                'localhost/test1:hello',
+                'localhost/test1:hello2'
+            ]
+        )
+
+    def test_site_background_jobs_exist(self):
+        site = zoom.sites.Site()
+        site.apps_paths.append(zoom.tools.zoompath('tests', 'unittests', 'apps'))
+        names = sorted([job.name for job in site.background_jobs])
+        self.assertEqual(
+            names,
+            ['sample:tick', 'test1:hello', 'test1:hello2', 'test2:hello']
+        )
