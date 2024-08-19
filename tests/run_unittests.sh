38480b78b29fa05245db23b3bf96c993018ba5b9
Herb Lainchbury <herb@dynamic-solutions.com>
2024-08-19 14:03:28 -0700

    attempt to get database connection going in test environment

--- tests/run_unittests.sh
+++ tests/run_unittests.sh
@@ -1,51 +1,53 @@
 #!/bin/bash
 
-pip install -U pip
-pip install .
-pip install -r tests/requirements.txt
-
-export PYTHONPATH=$(pwd)
 
 apt-get update -qq
 apt-get install -y mariadb-client
 
-mariadb -e "create user zoomuser identified by 'zoompass'"
-mariadb -e "grant all on *.* to zoomuser"
+mariadb -h mariadb -e "create user zoomuser identified by 'zoompass'"
+# mariadb -e "grant all on *.* to zoomuser"
 
-zoom database -e mysql -H mariadb -u zoomuser -p zoompass create zoomdata
-zoom database -e mysql -H mariadb -u zoomuser -p zoompass create zoomtest
 
-export ZOOM_TEST_DATABASE_HOST=mariadb
-export ZOOM_TEST_DATABASE_USER=zoomuser
-export ZOOM_TEST_DATABASE_PASSWORD=zoompass
+# pip install -U pip
+# pip install .
+# pip install -r tests/requirements.txt
 
-export ZOOM_DEFAULT_INSTANCE=$(pwd)/zoom/_assets/web
-export ZOOM_DEFAULT_SITE_INI=$ZOOM_DEFAULT_INSTANCE/sites/localhost/site.ini
+# export PYTHONPATH=$(pwd)
 
-export ZOOM_TEST_LOG=$ZOOM_DEFAULT_INSTANCE
+# zoom database -e mysql -H mariadb -u zoomuser -p zoompass create zoomdata
+# zoom database -e mysql -H mariadb -u zoomuser -p zoompass create zoomtest
 
-echo "host=mariadb" >> $ZOOM_DEFAULT_SITE_INI
-echo "user=zoomuser" >> $ZOOM_DEFAULT_SITE_INI
-echo "password=zoompass" >> $ZOOM_DEFAULT_SITE_INI
+# export ZOOM_TEST_DATABASE_HOST=mariadb
+# export ZOOM_TEST_DATABASE_USER=zoomuser
+# export ZOOM_TEST_DATABASE_PASSWORD=zoompass
 
-# get locales not included in base Python image
-apt-get update && apt-get install -y --no-install-recommends \
-    locales \
-    locales-all
+# export ZOOM_DEFAULT_INSTANCE=$(pwd)/zoom/_assets/web
+# export ZOOM_DEFAULT_SITE_INI=$ZOOM_DEFAULT_INSTANCE/sites/localhost/site.ini
 
-# set locale
-export LC_ALL=C
+# export ZOOM_TEST_LOG=$ZOOM_DEFAULT_INSTANCE
 
-cat $ZOOM_DEFAULT_SITE_INI
+# echo "host=mariadb" >> $ZOOM_DEFAULT_SITE_INI
+# echo "user=zoomuser" >> $ZOOM_DEFAULT_SITE_INI
+# echo "password=zoompass" >> $ZOOM_DEFAULT_SITE_INI
 
-# run tests
-pytest \
-    -x \
-    -v \
-    --doctest-modules \
-    --ignore=zoom/testing \
-    --ignore=zoom/cli \
-    --ignore=zoom/_assets \
-    --ignore=tests/unittests/apps \
-    zoom \
-    tests/unittests
+# # get locales not included in base Python image
+# apt-get update && apt-get install -y --no-install-recommends \
+#     locales \
+#     locales-all
+
+# # set locale
+# export LC_ALL=C
+
+# cat $ZOOM_DEFAULT_SITE_INI
+
+# # run tests
+# pytest \
+#     -x \
+#     -v \
+#     --doctest-modules \
+#     --ignore=zoom/testing \
+#     --ignore=zoom/cli \
+#     --ignore=zoom/_assets \
+#     --ignore=tests/unittests/apps \
+#     zoom \
+#     tests/unittests
