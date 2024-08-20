0c749b6bb9c923a2be68151eb7359a262ff8452c
Herb Lainchbury <herb@dynamic-solutions.com>
2024-08-20 13:52:15 -0700

    update scripts to run tests in docker

--- tests/run_docker_unittests.sh
+++ tests/run_docker_unittests.sh
@@ -3,50 +3,12 @@ apt-get update -qq
 
 echo "installing the database"
 
-# echo "127.0.0.1 mariadb" | tee -a /etc/hosts
 export DEBIAN_FRONTEND=noninteractive
 export MYSQL_ROOT_PASSWORD=root
 
-# apt-get update
-# apt-get install -y wget gnupg
-# wget -qO /etc/apt/trusted.gpg.d/mariadb_release_signing_key.asc https://mariadb.org/mariadb_release_signing_key.asc
-# echo "deb [arch=amd64,arm64,ppc64el] http://mirror.nodesdirect.com/mariadb/repo/10.7/debian bookworm main" > /etc/apt/sources.list.d/mariadb.list
-
-# apt-get install software-properties-common dirmngr -y
-# wget -qO /etc/apt/trusted.gpg.d/mariadb_release_signing_key.asc https://mariadb.org/mariadb_release_signing_key.asc
-# add-apt-repository -y 'deb [arch=amd64,arm64,ppc64el] http://mirror.nodesdirect.com/mariadb/repo/10.7/ubuntu focal main'
-
-# apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-# add-apt-repository -y 'deb [arch=amd64,arm64,ppc64el] http://mirror.nodesdirect.com/mariadb/repo/10.7/ubuntu focal main'
 apt-get update
 apt-get install -y mariadb-server
 
 service mariadb start
 
-# mysql -u root -e "SET PASSWORD FOR root@'localhost' = PASSWORD('root'); FLUSH PRIVILEGES;"
-
-# mysql -u root -proot -e "UPDATE mysql.user SET host='%' WHERE user='root'; FLUSH PRIVILEGES;"
-
-# bash
-
-# apt-get update -qq
-# apt-get install -y mariadb-server-10.7
-
-# # apt-get install -yqq mariadb-server
-
-# service mariadb start
-
-# bash
-
-# mysql -e "select host, user, password from mysql.user"
-
-# mysql -e "create user zoomuser identified by 'zoompass'"
-# mysql -e "grant all on *.* to zoomuser"
-
-# mysql -e "select host, user, password from mysql.user"
-
-# mysql -h mariadb -u zoomuser -pzoompass -e "show databases"
-
 source tests/run_unittests.sh
-
-# bash
--- tests/run_in_docker.sh
+++ tests/run_in_docker.sh
@@ -1,2 +1,25 @@
+#!/bin/bash
 
-docker run -v $(pwd):/work -it python:3.8 bash -c "bash /work/tests/run_docker_unittests.sh"
+VERBOSE=${VERBOSE:-0}
+VERSIONS=${VERSIONS:-"3.7 3.8 3.9 3.12"}
+
+# Loop through each Python version
+for VERSION in $VERSIONS
+do
+    echo "Running unittests in Python $VERSION"
+
+    # Run the Docker command and conditionally apply grep based on VERBOSE
+    if [ "$VERBOSE" -eq 1 ]; then
+        docker run \
+            -v $(pwd):/work \
+            -it python:$VERSION \
+            bash -c "bash /work/tests/run_docker_unittests.sh"
+    else
+        docker run \
+            -v $(pwd):/work \
+            -it python:$VERSION \
+            bash -c "bash /work/tests/run_docker_unittests.sh" \
+            | grep "===\ .*\ passed"
+    fi
+
+done
