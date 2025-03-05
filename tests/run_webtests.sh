f144e8432537d89d01dd8cf1c6a5f2de2be78c94
Herb Lainchbury <herb@dynamic-solutions.com>
2025-03-05 12:26:15 -0800

    revert to stable version of chrome for testing

--- tests/run_webtests.sh
+++ tests/run_webtests.sh
@@ -37,11 +37,11 @@ echo "host=$ZOOM_TEST_DATABASE_HOST" >> $ZOOM_DEFAULT_SITE_INI
 echo "user=zoomuser" >> $ZOOM_DEFAULT_SITE_INI
 echo "password=zoompass" >> $ZOOM_DEFAULT_SITE_INI
 
-# install google chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get -y update
-apt-get install -y google-chrome-stable
+# install google chrome 133.0.6943.141
+wget -P /tmp http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_133.0.6943.141-1_amd64.deb
+dpkg --force-all -i /tmp/google-chrome-stable_133.0.6943.141-1_amd64.deb || true
+apt-get update -qq
+apt-get install -f -y
 
 google-chrome --version
 
