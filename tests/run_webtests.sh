4790f5fb5adc236d8e07f76aa509cf1b2e06890b
Herb Lainchbury <herb@dynamic-solutions.com>
2025-03-05 15:08:06 -0800

    move chrome installation into separate script

--- tests/run_webtests.sh
+++ tests/run_webtests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# install google chrome 133.0.6943.141
-wget -P /tmp http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_133.0.6943.141-1_amd64.deb
-dpkg --force-all -i /tmp/google-chrome-stable_133.0.6943.141-1_amd64.deb || true
-apt-get update -qqq
-apt-get install -f -y -qqq
-
-google-chrome --version
+source install_chrome.sh
 
 apt-get update -qq
 apt-get install -y mariadb-client iputils-ping
